1a7507d3816c2ec250bfb7cc0fb03fbd3f239558
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-06-28 19:33:19 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,7 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          ></a>
         </div>
       </div>
 
