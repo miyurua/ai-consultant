f727d97577cb7af643b3cf90bf86eb7d2e9140e4
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-07-04 18:21:13 +0530

    changes to the home style

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ const Header = () => {
       <Link href={"/"}>
         <h1>Consultant AI</h1>
         <div>
-          <h2>
-            <span className="bg-slate-400">Digital Solutions</span> Tailored to
+          <h2 className="text-6xl md:text-9xl hidden">
+            <span className="text-white">Digital Solutions</span> Tailored to
             You!
           </h2>
           <h1></h1>
